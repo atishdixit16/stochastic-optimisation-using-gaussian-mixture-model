a188fbf9c3f2006915f99cbb2ed74a909b36da43
atishdixit16 <atishdixit16@gmail.com>
2018-03-02 11:31:15 +0530

    dummy commit

--- main.r
+++ main.r
@@ -3,10 +3,10 @@ source("multiVariateGMM.r" )
 rastrigin <- function(x) { #x is a trials-by-dim matrix
         A = 10
         n = ncol(x)
-        return ( apply( x, 1, function(i)  A*n + sum(i^2 + A*cos(2*pi*i))  )  )
+        return ( apply( x, 1, function(i)  A*n + sum(i^2 - A*cos(2*pi*i))  )  )
 }
 
 limMat <- matrix(c(-5,-5,5,5),2,2)
-sol <- gmmOpt(func=rastrigin, 10, 10, dim = 2, lim = limMat, display = TRUE) 
+sol <- gmmOpt(func=rastrigin, iter=100, trials=1000, dim = 2, lim = limMat, display = TRUE, wtUpdateMethod="exp") 
 
 print(sol)
