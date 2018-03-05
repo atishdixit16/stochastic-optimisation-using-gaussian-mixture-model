e3ec01fa42b6ae0296cf1a452a2d0ab7067b0812
atishdixit16 <atishdixit16@gmail.com>
2018-03-05 11:08:26 +0530

    Add rosenbrock function in main to add

--- main.r
+++ main.r
@@ -6,9 +6,13 @@ rastrigin <- function(x) { #x is a trials-by-dim matrix
         return ( apply( x, 1, function(i)  A*n + sum(i^2 - A*cos(2*pi*i))  )  )
 }
 
+rosenbrock <- function(x) { #x is a trials-by-dim matrix
+	n <- ncol(x)
+	return ( apply( x , 1 , function(i) sum (100*(i[1:(n-1)]^2 - i[2:n])^2 + (i[1:(n-1)] - 1)^2) ) )
+}
 
-dimN = 4
+dimN = 2
 limMat <- matrix(c(-5,5),dimN,2,byrow=T)
-sol <- gmmOpt(func=rastrigin, iter=200, trials=1000, dim = dimN, lim = limMat, display = TRUE, wtUpdateMethod="exp", StdDevWtUpdate='flat') 
+sol <- gmmOpt(func=rosenbrock, iter=300, trials=2000, dim = dimN, lim = limMat, display = TRUE, wtUpdateMethod="exp", StdDevWtUpdate='flat') 
 
 print(sol)
