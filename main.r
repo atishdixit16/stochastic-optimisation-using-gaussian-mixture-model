72131475e33f3a82ce302ec8b364c2d83e321a92
atishdixit16 <atishdixit16@gmail.com>
2018-03-05 12:15:36 +0530

    Add 2d linear system of equations

--- main.r
+++ main.r
@@ -11,8 +11,17 @@ rosenbrock <- function(x) { #x is a trials-by-dim matrix
 	return ( apply( x , 1 , function(i) sum (100*(i[1:(n-1)]^2 - i[2:n])^2 + (i[1:(n-1)] - 1)^2) ) )
 }
 
+linSys2 <- function(x) { #x is a trials-by-2 matrix (answer=4,1)
+	stopifnot(ncol(x) == 2 )
+	A <- matrix(c(2,1,-1,1),2,2)
+	b <- matrix(c(7,5), 2,1)
+	b_try <- apply(x,1,function(i) A%*%i )
+	ans <- apply(b_try,2, function(i) sqrt(sum((i-b)**2)/2)  )  #root mean square as objective function
+	return ( ans )
+}
+
 dimN = 2
 limMat <- matrix(c(-5,5),dimN,2,byrow=T)
-sol <- gmmOpt(func=rosenbrock, iter=300, trials=2000, dim = dimN, lim = limMat, display = TRUE, wtUpdateMethod="exp", StdDevWtUpdate='flat') 
+sol <- gmmOpt(func=linSys2, iter=300, trials=2000, dim = dimN, lim = limMat, display = TRUE, wtUpdateMethod="exp", StdDevWtUpdate='flat') 
 
 print(sol)
